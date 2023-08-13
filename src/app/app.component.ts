d1956f1abd022962eddd0976e33c3b8610f12aeb
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-08-13 17:17:02 -0400

    implementing rxjs observables, pipes and operators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
-import { Component } from '@angular/core';
-// import { Observable } from 'rxjs';
+import { Component, inject } from '@angular/core';
+import {
+  distinctUntilChanged,
+  filter,
+  forkJoin,
+  map,
+  mergeMap,
+  of,
+  Subject,
+  tap,
+} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +18,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'message-app';
+  name = 'Angular';
+  numberArray$ = of([1, 2, 3, 4, 5]);
+  valueSubject$ = new Subject<string>();
+
+  http = inject(HttpClient);
+  fromPokeApi$ = this.http
+    .get<{
+      count: number;
+      next: string;
+      previous: string | null;
+      results: { name: string; url: string }[];
+    }>(' https://pokeapi.co/api/v2/pokemon?limit=1')
+    .pipe(
+      mergeMap((mainResponse) => {
+        // Get the url property from each item in the results array from the main response. Creates an observable
+        const urlRequests = mainResponse.results.map((item) =>
+          this.http.get(item.url)
+        );
+        //Executes the observable
+        return forkJoin(urlRequests).pipe(
+          mergeMap((detailsResponses) => {
+            // Combine the results from the main response with the details for each item in the results array
+            const combinedResults = mainResponse.results.map(
+              (result, index) => {
+                return {
+                  ...result,
+                  details: detailsResponses[index],
+                };
+              }
+            );
+             // Combine the main response with the updated results (detailed data for each pokemon)
+            return [{ ...mainResponse, results: combinedResults }];
+          })
+        );
+      })
+    );
+
+  constructor() {
+    this.valueSubject$
+      .asObservable()
+      .pipe(
+        // for side effects
+        tap((val) => console.log('val from tap ' + val)),
+        // allowed condition passes
+        filter((val) => val !== ''),
+        // do not repeat value
+        distinctUntilChanged(),
+        // transforms value as specified
+        map((val) => val.toUpperCase())
+      )
+      .subscribe(console.log);
+  }
+
+  //Example of how you could execute logic without observables/pipes
+  valueSubjectAsFunction(val: string) {
+    // tap for side effects
+    console.log(val);
+    // filter
+    if (val === '') return;
+    //... so on
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { MessageListComponent } from './message-list/message-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    MessageListComponent
-  ],
+  declarations: [AppComponent, MessageListComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    RouterModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

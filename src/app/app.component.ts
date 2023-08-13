b8e7a5fd5682697f7e7de593586fb7e3068e7cb5
Cassandra Curcio <cassandra.curcio@gmail.com>
2023-08-13 17:00:25 -0400

    remove commented code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,30 +8,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'message-app';
-  // messages = new Observable((observer) => {
-  //   observer.next('Hello');
-  //   observer.error('Error receiving the message');
-  //   return {
-  //     unsubscribe() {
-  //       console.log('No more messages');
-  //     },
-  //   };
-  // });
-
-  // messageSubscription = this.messages.subscribe({
-  //   next(message) {
-  //     console.log(`Message: ${message}`);
-  //   },
-  //   error(err) {
-  //     console.log(`Error: ${err}`);
-  //   },
-  // });
-
-
 }
-
-    // setTimeout(() => {
-    //   messageSubscription.unsubscribe();
-    // }, 10000);
-
-
